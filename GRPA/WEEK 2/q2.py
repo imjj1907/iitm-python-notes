a76160705e1ac5f278e3a60096bcf2f882518f2d
josh <josh1906007@gmail.com>
2025-02-16 15:23:13 +0530

    Update code changes

---
+++ GRPA/WEEK 2/q2.py
@@ -0,0 +1,15 @@
+'''STRING ESCAPING ''' # This is the second question in the assignment WEEK 2
+
+#ANSWER
+
+# A single quote ' and a double quote "
+output1 = 'A single quote \' and a double quote "'
+
+# A forward slash / and a backward slash \
+output2 = 'A forward slash / and a backward slash \\' 
+
+# Three single quotes ''' and three double quotes """
+output3 = "Three single quotes ''' and three double quotes \"\"\"" 
+
+# Double forward slash // and Double backward slash \\
+output4 = "Double forward slash // and Double backward slash \\\\"
---
+++ NOTES/README.md
@@ -0,0 +1,22 @@
+# iitm-python-notes 
+Python Notes and Graded Programming Assignments
+
+This repository contains Python notes and graded programming assignments from the IITM BS Degree Program. It serves as a collection of learning resources and solutions to programming tasks from the course.
+
+LEARN AND REVISE AS YOU RUN THE CODE 
+#COVER EVERY SINGLE TOPICS TAUGHT IN THE LECTURES , KEEP READING AS YOU COMPILE 
+
+
+
+CONTACT DETAILS 
+
+EMAIL: iitbsjo@gmail.com
+
+YOUTUBE CHANNEL : https://www.youtube.com/@IITbs.19
+
+
+
+
+
+
+
